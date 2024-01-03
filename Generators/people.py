ef45727be9d0ffc7d9068335289045b509861199
YoungjuneKim <oracle99i@gmail.com>
2024-01-03 22:18:15 +0000

    python2 -> python3 변경 및 일부 수정

--- Generators/people.py
+++ Generators/people.py
@@ -5,35 +5,27 @@ import time
 names = ['John', 'Corey', 'Adam', 'Steve', 'Rick', 'Thomas']
 majors = ['Math', 'Engineering', 'CompSci', 'Arts', 'Business']
 
-print 'Memory (Before): {}Mb'.format(mem_profile.memory_usage_psutil())
+print ('Memory (Before): {}Mb'.format(mem_profile.memory_usage_psutil()))
 
 def people_list(num_people):
     result = []
-    for i in xrange(num_people):
-        person = {
-                    'id': i,
-                    'name': random.choice(names),
-                    'major': random.choice(majors)
-                }
+    for i in range(num_people):
+        person = {'id': i, 'name': random.choice(names), 'major': random.choice(majors)}
         result.append(person)
     return result
 
 def people_generator(num_people):
-    for i in xrange(num_people):
-        person = {
-                    'id': i,
-                    'name': random.choice(names),
-                    'major': random.choice(majors)
-                }
+    for i in range(num_people):
+        person = {'id': i, 'name': random.choice(names),'major': random.choice(majors)}
         yield person
 
-# t1 = time.clock()
+# t1 = time.time()
 # people = people_list(1000000)
-# t2 = time.clock()
+# t2 = time.time()
 
-t1 = time.clock()
+t1 = time.time()
 people = people_generator(1000000)
-t2 = time.clock()
+t2 = time.time()
 
-print 'Memory (After) : {}Mb'.format(mem_profile.memory_usage_psutil())
-print 'Took {} Seconds'.format(t2-t1)
+print ('Memory (After) : {}Mb'.format(mem_profile.memory_usage_psutil()))
+print ('Took {} Seconds'.format(t2-t1))
